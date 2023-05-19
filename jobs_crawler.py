6794d62521ffb878f7f5e32baca5a40bc15e8653
Michael Williams <michaelgwilliams774@gmail.com>
2023-05-18 20:31:10 -0400

    Add scrape the basic data of the recent job posts

--- jobs_crawler.py
+++ jobs_crawler.py
@@ -25,6 +25,7 @@ set_logger(logger)
 ## Set Chrome Driver Options ##
 
 chrome_options = Options()
+chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 # chrome_options.add_argument("--incognito")
 chrome_options.add_argument('--ignore-ssl-errors=yes')
 chrome_options.add_argument('--ignore-certificate-errors')
@@ -32,28 +33,32 @@ chrome_options.add_argument('--ignore-certificate-errors')
 # chrome_options.add_argument('--profile-directory=Profile 1'),
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--headless") # This won't show the window
-chrome_options.binary_location = "/usr/bin/google-chrome-stable"
+# chrome_options.add_argument("--headless") # This won't show the window
+chrome_options.page_load_strategy = 'normal'
+# chrome_options.binary_location = "/usr/bin/google-chrome-stable"
 
 print('Enter keyword: ')
 keyword = input()
 
 url = f"https://www.upwork.com/nx/jobs/search/?q={keyword}&sort=recency"
+# url = f"https://www.upwork.com"
 # url = f'https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi'
 
 driver = webdriver.Chrome(options=chrome_options)
 
 driver.get(url)
+driver.add_cookie({"name":"cookie_domain", "value": ".upwork.com"})
+driver.add_cookie({"name":"lang", "value": "en"})
+driver.add_cookie({"name":"IR_gbd", "value": "upwork.com"})
 time.sleep(60)
-
-counter = 0
 
 ## Check if the Upwork website is reached out
 logger.info(driver.title)
 
 def jobs_crawler():
+    counter = 0
     #open document and start writing process
-    with open('UpworkJobs.csv','a+', encoding="utf-8", newline='') as fd:
+    with open('./UpworkJobs.csv','a', encoding="utf-8", newline='') as fd:
         csv_writer = writer(fd, delimiter=",")
     
         # write title
@@ -64,10 +69,9 @@ def jobs_crawler():
                     ])
     
         while True:
-    
+            logger.info("Page reading...")
             # wait content to be loaded
-            titleContent = WebDriverWait(driver, 300).until(EC.presence_of_element_located((By.CSS_SELECTOR, "h3[class='my-0 p-sm-right job-tile-title'] > a")))
-            logger.info(f"title: {titleContent.getText()}")
+            titleContent = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "h3[class='my-0 p-sm-right job-tile-title'] > a")))
             
             # detect next button
             nextButton = driver.find_elements(By.CSS_SELECTOR, "button[class='up-pagination-item up-btn up-btn-link'] > div[class='next-icon up-icon']")
@@ -78,6 +82,9 @@ def jobs_crawler():
             contractorTier = driver.find_elements(By.CSS_SELECTOR, "span[data-test='contractor-tier']")
             # write data to document
             for el in range(len(jobTitle)):
+                logger.info(jobTitle[el].text)
+                logger.info(jobType[el].text)
+                logger.info(contractorTier[el].text)
                 csv_writer.writerow([
                     jobTitle[el].text,
                     jobType[el].text,
@@ -95,5 +102,5 @@ def jobs_crawler():
             counter += 1
             logger.info('page: ' + str(counter))
 
-            if (counter == 5):
+            if (counter == 2):
                 break
